2b76f317685c53b23cee1b0b05c6f0eb8766db2d
kiran1228 <kiran@jauntvr.com>
2019-04-04 17:01:27 -0500

    add linux compat file

---
+++ UVAtlas/linux/UVAtlasTypes.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#ifdef __LINUX__
+
+#ifndef _HRESULT_DEFINED
+#define _HRESULT_DEFINED
+typedef long HRESULT;
+#endif // !_HRESULT_DEFINED
+
+//------------------------------------------------------------------------------
+// 2D Vector; 32 bit floating point components
+struct XMFLOAT2
+{
+    float x;
+    float y;
+
+    XMFLOAT2() = default;
+
+    XMFLOAT2(const XMFLOAT2&) = default;
+    XMFLOAT2& operator=(const XMFLOAT2&) = default;
+
+    XMFLOAT2(XMFLOAT2&&) = default;
+    XMFLOAT2& operator=(XMFLOAT2&&) = default;
+
+    XM_CONSTEXPR XMFLOAT2(float _x, float _y) : x(_x), y(_y) {}
+    explicit XMFLOAT2(_In_reads_(2) const float* pArray) : x(pArray[0]), y(pArray[1]) {}
+};
+
+//------------------------------------------------------------------------------
+// 3D Vector; 32 bit floating point components
+struct XMFLOAT3
+{
+    float x;
+    float y;
+    float z;
+
+    XMFLOAT3() = default;
+
+    XMFLOAT3(const XMFLOAT3&) = default;
+    XMFLOAT3& operator=(const XMFLOAT3&) = default;
+
+    XMFLOAT3(XMFLOAT3&&) = default;
+    XMFLOAT3& operator=(XMFLOAT3&&) = default;
+
+    XM_CONSTEXPR XMFLOAT3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+    explicit XMFLOAT3(_In_reads_(3) const float* pArray) : x(pArray[0]), y(pArray[1]), z(pArray[2]) {}
+};
+
+
+
+#endif //__LINUX__
